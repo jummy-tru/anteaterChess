a5a67dc0ab776e21cb3e265fc12a7219143f881e
SunNinjs <coolmanpower1@gmail.com>
2026-04-13 14:17:02 -0700

    Changed board.c and board.h to include FEN board state adaption

--- board.c
+++ board.c
@@ -2,9 +2,10 @@
 // displaying the contents of the board
 
 #include <stdio.h>
+#include <ctype.h>
 #include "board.h"
 
-void emptyBoard(Board* board)
+void emptyBoard(Board *board)
 {
   for (int i = 0; i < 8; i++)
   {
@@ -16,185 +17,224 @@ void emptyBoard(Board* board)
   }
 }
 
-void setupBoard(Board* board)
+static Piece makePiece(PieceType type, Color color)
 {
-  // Empty current board
-  emptyBoard(board);
-
-  // Initialize white pawns
-  for (int i = 0; i < 10; i++)
-  {
-    // A through J 2
-    board->squares[1][i].color = WHITE;
-    board->squares[1][i].pieceType = PAWN;
-  }
-
-  // Initialize white king
-  // F1 (row 1, column 6)
-  board->squares[0][5].color = WHITE;
-  board->squares[0][5].pieceType = KING;
-
-  // Initialize white queen
-  // E1 (row 1, column 5)
-  board->squares[0][4].color = WHITE;
-  board->squares[0][4].pieceType = QUEEN;
-
-  // Initialize white anteaters
-  // D1 and G1 (row 1, columns 4 and 7)
-  board->squares[0][3].color = WHITE;
-  board->squares[0][3].pieceType = ANTEATER;
-  board->squares[0][6].color = WHITE;
-  board->squares[0][6].pieceType = ANTEATER;
-
-  // Initialize white bishops
-  // C1 and H1 (row 1, columns 3 and 8)
-  board->squares[0][2].color = WHITE;
-  board->squares[0][2].pieceType = BISHOP;
-  board->squares[0][7].color = WHITE;
-  board->squares[0][7].pieceType = BISHOP;
-
-  // Initialize white knights
-  // B1 and I1 (row 1, columns 2 and 9)
-  board->squares[0][1].color = WHITE;
-  board->squares[0][1].pieceType = KNIGHT;
-  board->squares[0][8].color = WHITE;
-  board->squares[0][8].pieceType = KNIGHT;
-
-  //Initialize white rooks
-  // A1 and J1 (row 1, columns 1 and 10)
-  board->squares[0][0].color = WHITE;
-  board->squares[0][0].pieceType = ROOK;
-  board->squares[0][9].color = WHITE;
-  board->squares[0][9].pieceType = ROOK;
-
-  // Initialize black pawns
-  for (int i = 0; i < 10; i++)
-  {
-    // A through J 7
-    board->squares[6][i].color = BLACK;
-    board->squares[6][i].pieceType = PAWN;
-  }
-
-  // Initialize black king
-  // F8 (row 8, column 6)
-  board->squares[7][5].color = BLACK;
-  board->squares[7][5].pieceType = KING;
-
-  // Initialize black queen
-  // E8 (row 8, column 5)
-  board->squares[7][4].color = BLACK;
-  board->squares[7][4].pieceType = QUEEN;
-
-  // Initialize black anteaters
-  // D8 and G8 (row 8, columns 4 and 7)
-  board->squares[7][3].color = BLACK;
-  board->squares[7][3].pieceType = ANTEATER;
-  board->squares[7][6].color = BLACK;
-  board->squares[7][6].pieceType = ANTEATER;
-
-  // Initialize black bishops
-  // C8 and H8 (row 8, columns 3 and 8)
-  board->squares[7][2].color = BLACK;
-  board->squares[7][2].pieceType = BISHOP;
-  board->squares[7][7].color = BLACK;
-  board->squares[7][7].pieceType = BISHOP;
-
-  // Initialize black knights
-  // B8 and I8 (row 8, columns 2 and 9)
-  board->squares[7][1].color = BLACK;
-  board->squares[7][1].pieceType = KNIGHT;
-  board->squares[7][8].color = BLACK;
-  board->squares[7][8].pieceType = KNIGHT;
-
-  // Initialize black rooks
-  // A8 and J8 (row 8, columns 1 and 10)
-  board->squares[7][0].color = BLACK;
-  board->squares[7][0].pieceType = ROOK;
-  board->squares[7][9].color = BLACK;
-  board->squares[7][9].pieceType = ROOK;
+  Piece p;
+  p.pieceType = type;
+  p.color = color;
+  return p;
 }
 
-// Returns the piece object that resides on a particular square 
-// (can limit permisions eventually 
+static char pieceTypeChar(PieceType type)
+{
+  switch (type)
+  {
+  case KING:
+    return 'K';
+  case QUEEN:
+    return 'Q';
+  case ROOK:
+    return 'R';
+  case BISHOP:
+    return 'B';
+  case KNIGHT:
+    return 'N';
+  case PAWN:
+    return 'P';
+  case ANTEATER:
+    return 'A';
+  default:
+    return '.';
+  }
+}
+
+static char pieceColorChar(Color color)
+{
+  switch (color)
+  {
+  case WHITE:
+    return 'w';
+  case BLACK:
+    return 'b';
+  default:
+    return '.';
+  }
+}
+
+static Piece pieceFromChar(char ch)
+{
+  Color color;
+  PieceType type;
+
+  if (isupper((unsigned char)ch))
+  {
+    color = WHITE;
+  }
+  else
+  {
+    color = BLACK;
+  }
+
+  switch (tolower((unsigned char)ch))
+  {
+  case 'k':
+    type = KING;
+    break;
+  case 'q':
+    type = QUEEN;
+    break;
+  case 'r':
+    type = ROOK;
+    break;
+  case 'b':
+    type = BISHOP;
+    break;
+  case 'n':
+    type = KNIGHT;
+    break;
+  case 'p':
+    type = PAWN;
+    break;
+  case 'a':
+    type = ANTEATER;
+    break;
+  default:
+    return makePiece(EMPTY, NONE);
+  }
+
+  return makePiece(type, color);
+}
+
+int loadBoardFromFEN(Board *board, const char *fen)
+{
+  int row = 0;
+  int col = 0;
+  int i = 0;
+
+  for (row = 0; row < ROWS; row++)
+  {
+    for (col = 0; col < COLS; col++)
+    {
+      board->squares[row][col] = makePiece(EMPTY, NONE);
+    }
+  }
+
+  row = 0;
+  col = 0;
+
+  while (fen[i] != '\0' && row < ROWS)
+  {
+    char ch = fen[i];
+
+    if (ch == '/')
+    {
+      if (col != COLS)
+      {
+        return 0;
+      }
+      row++;
+      col = 0;
+    }
+    else if (isdigit((unsigned char)ch))
+    {
+      int emptyCount = 0;
+
+      while (isdigit((unsigned char)fen[i]))
+      {
+        emptyCount = emptyCount * 10 + (fen[i] - '0');
+        i++;
+      }
+      i--;
+
+      if (col + emptyCount > COLS)
+      {
+        return 0;
+      }
+
+      col += emptyCount;
+    }
+    else
+    {
+      Piece p = pieceFromChar(ch);
+      if (p.pieceType == EMPTY)
+      {
+        return 0;
+      }
+      if (col >= COLS)
+      {
+        return 0;
+      }
+
+      board->squares[row][col] = p;
+      col++;
+    }
+
+    i++;
+  }
+
+  if (row != ROWS - 1 || col != COLS)
+  {
+    return 0;
+  }
+
+  return 1;
+}
+
+// Loads the board using the FEN notation for a board state
+void setupBoard(Board *board)
+{
+  // String Representing the starting state
+  const char *startingFEN = "rnbaqkanbr/pppppppppp/10/10/10/10/PPPPPPPPPP/RNBAQKANBR";
+
+  loadBoardFromFEN(board, startingFEN);
+}
+
+// Returns the piece object that resides on a particular square
+// (can limit permisions eventually
 // or have it return a char to simulate private/public)
-Piece getPiece(Board* board, int row, int column)
+Piece getPiece(Board *board, int row, int column)
 {
   return board->squares[row][column];
 }
 
-void movePiece(Board* board, int fromRow, int fromColumn, 
-                int toRow, int toColumn)
+void movePiece(Board *board, int fromRow, int fromColumn, int toRow, int toColumn)
 {
   // Function in charge of moving piece from some place to some place
-  // Validation should have already happened in moves so 
-  //you don't need validation here
+  // Validation should have already happened in moves so
+  // you don't need validation here
 }
 
-// Function replaces a piece during exchanges or promotions or removes 
+// Function replaces a piece during exchanges or promotions or removes
 // it in case of En Passant or anteater captures
-void replacePiece(Board* board, int row, int column, PieceType newPiece)
+void replacePiece(Board *board, int row, int column, PieceType newPiece)
 {
-  
 }
 
-void showBoard(Board* board)
+void showBoard(Board *board)
 {
-  char pieceColorText[8][10]; // char array storing w, b, or ' ' (none)
-  char pieceTypeText[8][10]; // char array storing R,N,B,A,Q,K,P or ' ' (none)
-  for (int i = 0; i < 8; i++)
+  int r, c;
+
+  printf("\n    A   B   C   D   E   F   G   H   I   J\n");
+  for (r = 0; r < ROWS; r++)
   {
-    for (int j = 0; j < 10; j++)
+    printf("  +---+---+---+---+---+---+---+---+---+---+\n");
+    printf("%d |", 8 - r);
+
+    for (c = 0; c < COLS; c++)
     {
-      Piece currentPiece = getPiece(board, i, j);
-      if (currentPiece.color == BLACK)
+      Piece p = board->squares[r][c];
+      if (p.pieceType == EMPTY)
       {
-        pieceColorText[i][j] = 'b';
-      }
-      else if (currentPiece.color == WHITE)
-      {
-        pieceColorText[i][j] = 'w';
+        printf("   |");
       }
       else
       {
-        pieceColorText[i][j] = ' ';
-      }
-
-      switch (currentPiece.pieceType)
-      {
-      case PAWN:
-        pieceTypeText[i][j] = 'P';
-        break;
-      case ROOK:
-        pieceTypeText[i][j] = 'R';
-        break;
-      case KNIGHT:
-        pieceTypeText[i][j] = 'N';
-        break;
-      case BISHOP:
-        pieceTypeText[i][j] = 'B';
-        break;
-      case ANTEATER:
-        pieceTypeText[i][j] = 'A';
-        break;
-      case QUEEN:
-        pieceTypeText[i][j] = 'Q';
-        break;
-      case KING:
-        pieceTypeText[i][j] = 'K';
-        break;
-      default:
-        pieceTypeText[i][j] = ' ';
+        printf(" %c%c|", pieceColorChar(p.color), pieceTypeChar(p.pieceType));
       }
     }
-  }
 
-  for (int i = 8; i > 0; i--)
-  {
-    printf("  +----+----+----+----+----+----+----+----+----+----+\n");
-    // if yall can think of a better way to refactor this be my guest.
-    // it works tho! dont break it!
-    printf("%i | %c%c | %c%c | %c%c | %c%c | %c%c | %c%c | %c%c | %c%c | %c%c | %c%c |\n", i, pieceColorText[i-1][0], pieceTypeText[i-1][0], pieceColorText[i-1][1], pieceTypeText[i-1][1], pieceColorText[i-1][2], pieceTypeText[i-1][2], pieceColorText[i-1][3], pieceTypeText[i-1][3], pieceColorText[i-1][4], pieceTypeText[i-1][4], pieceColorText[i-1][5], pieceTypeText[i-1][5], pieceColorText[i-1][6], pieceTypeText[i-1][6], pieceColorText[i-1][7], pieceTypeText[i-1][7], pieceColorText[i-1][8], pieceTypeText[i-1][8], pieceColorText[i-1][9], pieceTypeText[i-1][9]);
+    printf(" %d\n", 8 - r);
   }
-  printf("  +----+----+----+----+----+----+----+----+----+----+\n");
-  printf("    A    B    C    D    E    F    G    H    I    J\n");
+  printf("  +---+---+---+---+---+---+---+---+---+---+\n");
+  printf("    A   B   C   D   E   F   G   H   I   J\n\n");
 }
--- board.h
+++ board.h
@@ -3,12 +3,16 @@
 
 #include "pieces.h"
 
+#define ROWS 8
+#define COLS 10
+
 typedef struct {
   Piece squares[8][10];
 } Board;
 
 void emptyBoard(Board* board);
 void setupBoard(Board* board);
+int loadBoardFromFEN(Board *board, const char *fen);
 Piece getPiece(Board* board, int row, int column);
 void movePiece(Board* board, int fromRow, int fromColumn, 
                 int toRow, int toColumn);
