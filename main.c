a6c3feb3dd85cedb95a487d6d5d3b13e08f65dcf
jqTruong24 <jimmytruongq@gmail.com>
2026-04-14 15:31:41 -0700

    Added instruction for loop

--- main.c
+++ main.c
@@ -11,6 +11,26 @@ int main()
   setupBoard(boardPtr);
   showBoard(boardPtr);
 
+/*Control Flow Loop
+1. Ask user to input moves
+2. input validation (check if user input proper notations)
+3. isPieceCheck (check if there exist a piece)
+4. Piece color check( make sure the piece is the proper color)
+5. Check which piece type is it
+6. Check if the move is possible for the corresponding Piece type
+7. Check if the move is valid via Rule Check (ex: if the move make the king in check)
+IF ANY VALIDATION FAIL LOOP BACK TO 1 AND ASK USER AGAIN
+8. If all validation is correct then move is legal --> execute the move
+9. Check if move remove a piece from the board
+    9.2. if true then remove piece first
+10. Check if the move is one of the special move(castling, promotion, en passant, etc. )
+    10.2 If the move is promotion, change the piece type, then update the move with the move
+11. Execute the move
+12. Check if checkmate/stalemate
+    12.2 If true kill the loop and end the game (END LOOP)
+13. Swith Color
+LOOP BACK TO 1
+*/
 
   return 0;
 
