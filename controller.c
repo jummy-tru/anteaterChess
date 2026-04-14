d57341e6e1f5576ff297c80c2ce5caf407725c6a
matthew7lai <matthew7lai@gmail.com>
2026-04-14 11:43:44 -0700

    initialized the clock module

--- controller.c
+++ controller.c
@@ -1,12 +1,17 @@
 // File in charge of control flow (game start, menuing, quit game, etc)
 #include "controller.h"
+#include "clock.h"
 
 void gameStart()
 {
+  init_clock(60);
 
+  while (!is_time_up()) {
+    update_clock();
 }
 
 void gameEnd()
 {
   
 }
+gameEnd();
